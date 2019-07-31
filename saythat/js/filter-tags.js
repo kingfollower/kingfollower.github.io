14bada62314f15d78bc15b6dfee9948ee62abb30
Josh Bissell <contact@mrbissell.com>
2019-07-31 11:18:53 +0100

    Update filter-tags.js

--- saythat/js/filter-tags.js
+++ saythat/js/filter-tags.js
@@ -32,12 +32,12 @@
 
   $.each(tagged, function(tagName) {              
     $('<button/>', {                              
-      text: tagName + ' (' + ‘tagged[tagName].length + ')',  
+      text: tagName + ' (' + tagged[tagName].length + ')',  
       click: function() {                         
         $(this)                                   
           .addClass('active')                    
           .siblings()                             
-          .removeClass(‘active’);                 
+          .removeClass('active');                  
         $imgs                                     
           .hide()                                  
           .filter(tagged[tagName])               
